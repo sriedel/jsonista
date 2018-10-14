6a0cef3eca28efd864f89c81622ddb542a29ebd4
Sven Riedel <sr@gimp.org>
2018-10-14 16:53:47 +0200

    Expand functional test to include tests for local variables

--- test/functional/rendering_subtemplates_test.rb
+++ test/functional/rendering_subtemplates_test.rb
@@ -5,7 +5,8 @@ describe "Rendering a template that in turn renders partials" do
   let(:main_template_path) { File.join( fixture_path, "template_with_partials.jsonista" ) }
   let(:expected_result_structure) do
     { :key_1 => "value_in_main_template",
-      :key_2 => "value_in_partial" }
+      :key_2 => "value_in_partial",
+      :key_3 => [ 1, 2, nil, "foo", :bar ] }
   end
   let(:expected_result) { expected_result_structure.to_json }
 
